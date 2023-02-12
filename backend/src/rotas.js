a35c85ca0dfee3f2f9d5d36d0405ed5def2e1ab9
Dave <d4v3.oliveira@gmail.com>
2023-02-12 19:03:34 -0300

    FEATURE-03_login: implementado rota para login

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const cadastrarUsuario = require("./controladores/cadastrarUsuario.js");
+const login = require("./controladores/login.js");
 
 app.get("/", (req, res) => {
   res.json("ready to code!");
 });
 
 app.post("/cadastrar/usuario", cadastrarUsuario);
+app.post("/login", login);
 
 module.exports = app;
