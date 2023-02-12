58d030795c103c39f2f7e65c5b78084d03866dde
Dave <d4v3.oliveira@gmail.com>
2023-02-12 17:32:41 -0300

    FEATURE-02_cadastrar-usuario: inclusão de arquivo de conexão usando o Knex e da rota responsável por fazer o cadastro do usuario

---
+++ backend/src/controladores/cadastrarUsuario.js
@@ -0,0 +1,36 @@
+const knex = require("../configuracoes/conexao");
+const bcrypt = require("bcrypt");
+
+const cadastrarUsuario = async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  try {
+    if (!nome || !email || !senha) {
+      return res.status(400).json("informe todos os campos");
+    }
+
+    const verificarEmailRepetido = await knex("usuarios").where("email", email);
+
+    if (verificarEmailRepetido.length !== 0) {
+      return res.status(400).json("E-mail em uso");
+    }
+
+    const senha_cript = await bcrypt.hash(process.env.SENHA_BCRYPT, 10);
+
+    const usuarioCadastrado = await knex("usuarios").insert({
+      nome,
+      email,
+      senha: senha_cript,
+    });
+
+    if (!usuarioCadastrado) {
+      return res.status(500).json("Erro interno");
+    }
+
+    return res.status(201).json("Usuario cadastrado com sucesso!");
+  } catch (error) {
+    return res.status(500).json("Erro interno");
+  }
+};
+
+module.exports = cadastrarUsuario;
