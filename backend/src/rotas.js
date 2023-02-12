893a4993d103876dbb0676de9648d4bfc0f1e2c8
Dave <d4v3.oliveira@gmail.com>
2023-02-12 17:34:58 -0300

    FEATURE-02_cadastrar-usuario: criado arquivo principal de rotas

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cadastrarUsuario = require("./controladores/cadastrarUsuario.js");
 
 app.get("/", (req, res) => {
   res.json("ready to code!");
 });
+
+app.post("/cadastrar/usuario", cadastrarUsuario);
 
 module.exports = app;
