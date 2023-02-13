72f1bf1fe1a7b0008099e9e9c64224e359b61d54
Dave <d4v3.oliveira@gmail.com>
2023-02-13 11:14:56 -0300

    FEATURE-04_dashboard: Feito teste em rota com proteção

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const cadastrarUsuario = require("./controladores/cadastrarUsuario.js");
 const login = require("./controladores/login.js");
+const verificadorLogin = require("./intermediarios/verificadorLogin.js");
+
+app.post("/cadastrar/usuario", cadastrarUsuario);
+app.post("/login", login);
+
+app.use(verificadorLogin);
 
 app.get("/", (req, res) => {
   res.json("ready to code!");
 });
-
-app.post("/cadastrar/usuario", cadastrarUsuario);
-app.post("/login", login);
 
 module.exports = app;
