60b9522cc54896b80e64b03f37d2abca1e06dff2
Dave <d4v3.oliveira@gmail.com>
2023-02-15 11:27:24 -0300

    FEATURE-05_atualizar-usuario: implementado controlador

--- backend/src/controladores/atualizarUsuario.js
+++ backend/src/controladores/atualizarUsuario.js
@@ -4,10 +4,19 @@ const jwt = require("jsonwebtoken");
 
 const atualizarUsuario = async (req, res) => {
   const { nome, email, senha, novaSenha } = req.body;
+  let numeroDeCamposAlterados = 0;
+
+  if (nome || email || novaSenha) {
+    numeroDeCamposAlterados = 1;
+  }
 
   try {
     if (!senha) {
       return res.status(400).json("Informe a senha");
+    }
+
+    if (numeroDeCamposAlterados === 0) {
+      return res.status(400).json("Informe os campos a ser alterados");
     }
 
     const tokenJWT = await jwt.verify(
@@ -31,9 +40,18 @@ const atualizarUsuario = async (req, res) => {
       senha: novaSenha ? novaSenhaCripto : usuario[0].senha,
     };
 
-    console.log(atualizarDados);
+    const queryAtualizarUsuario = await knex("usuarios")
+      .where({ usuario_id: tokenJWT.id })
+      .update(
+        {
+          nome: atualizarDados.nome,
+          email: atualizarDados.email,
+          senha: atualizarDados.senha,
+        },
+        ["nome", "email"]
+      );
 
-    return res.json();
+    return res.json(queryAtualizarUsuario);
   } catch (error) {
     console.log(error);
     return res.status(500).json("Erro interno");
