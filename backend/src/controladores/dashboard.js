45691bce9f1b4cdeca90d4905973973261a9a3f4
Dave <d4v3.oliveira@gmail.com>
2023-02-16 12:12:23 -0300

    FEATURE-08_Filtros-dashboard: alterações no controlador de dashboard para usar os parametros de rota(caso vier)

--- backend/src/controladores/dashboard.js
+++ backend/src/controladores/dashboard.js
@@ -8,6 +8,19 @@ const dashboard = async (req, res) => {
       process.env.SENHA_JWT
     );
 
+    const { titulo } = req.params;
+
+    if (titulo) {
+      const retornoPesquisa = await knex("blog")
+        .where("referencia_usuario", decodeToken.id)
+        .whereILike("titulo", `%${req.params.titulo}%`);
+
+      if (retornoPesquisa.length === 0) {
+        return res.status(400).json("blog não encontrado");
+      }
+      return res.json(retornoPesquisa);
+    }
+
     const blogsEncontrados = await knex("blog").where(
       "referencia_usuario",
       decodeToken.id
